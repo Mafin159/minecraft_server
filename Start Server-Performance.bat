503456bc79c100ca43813ba903ab24c6418449e0
Valentin <WTrol25@gmail.com>
2019-07-07 19:34:19 +0200

    07072019_1933

--- Start Server-Performance.bat
+++ Start Server-Performance.bat
@@ -1,3 +1,3 @@
-java -server -XX:+UseG1GC -Xmx4G -Xms4G -Dsun.rmi.dgc.server.gcInterval=2147483646 -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -jar forge-1.12.2-14.23.5.2824-universal.jar nogui
+java -server -XX:+UseG1GC -Xmx6G -Xms6G -Dsun.rmi.dgc.server.gcInterval=2147483646 -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M -jar forge-1.12.2-14.23.5.2824-universal.jar nogui
 Pause >nul
 
